ba683b72fc0df2315ca138fd8390e03b3ba93af3
lgf-136 <86616706+lgf-136@users.noreply.github.com>
2022-09-19 13:17:17 +0800

    function is ok

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Router } from '@reach/router';
+import { RecoilRoot } from 'recoil';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import ErrorBoundary from './ErrorBoundary';
+import { NotFound } from './NotFound';
+import { Routes } from './dataStructure';
+
+interface Props {
+  path: Routes;
+}
+const Controller: React.FC<Props> = ({ path }) => <App path={path} />;
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ErrorBoundary>
+    <RecoilRoot>
+      <Router>
+        <Controller path="/" />
+        <Controller path="/active" />
+        <Controller path="/completed" />
+        <NotFound default />
+      </Router>
+    </RecoilRoot>
+  </ErrorBoundary>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
